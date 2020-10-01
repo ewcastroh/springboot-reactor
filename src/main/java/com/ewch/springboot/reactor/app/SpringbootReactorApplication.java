0ca476877454ce8dbca925bd0f6d9cdfd876d8dd
eimer-castro-g <eimer.castro@globant.com>
2020-10-01 13:38:51 -0500

    Adding User object to work in Flux streams. using map operator to convert values.

--- src/main/java/com/ewch/springboot/reactor/app/SpringbootReactorApplication.java
+++ src/main/java/com/ewch/springboot/reactor/app/SpringbootReactorApplication.java
@@ -1,5 +1,6 @@
 package com.ewch.springboot.reactor.app;
 
+import com.ewch.springboot.reactor.app.model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
@@ -19,17 +20,27 @@ public class SpringbootReactorApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		String names[] = {"Eimer", "Alejandra", "Manuel", "Luisa", "Duvan"};
-		Flux<String> namesStream = Flux.just(names)
-			.doOnNext(name -> {
-				if(name.isEmpty()) {
+		Flux<User> namesStream = Flux.just(names)
+			// Using map operator to convert a String in a User object
+			.map(name -> new User(name.toUpperCase(), null))
+			.doOnNext(user -> {
+				if(user == null) {
 					throw new RuntimeException("Names cannot be empty.");
 				}
-				System.out.println(name);
-			});
+				System.out.println(user.getFirstName());
+			})
+			// Using map operator to transform a User's attribute value into another one (String to lowercase)
+			.map(user -> {
+				user.setFirstName(user.getFirstName().toLowerCase());
+				return user;
+				});
 
 		namesStream.subscribe(
-			name -> LOGGER.info(name),
+			// Main method  to execute in callback
+			user -> LOGGER.info(user.toString()),
+			// Error handler
 			error -> LOGGER.error(error.getMessage()),
+			// onComplete method to execute when callback finishes
 			() -> LOGGER.info("Execution has finished successfully!")
 		);
 	}
