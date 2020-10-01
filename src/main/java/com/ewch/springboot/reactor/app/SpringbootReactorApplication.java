4f665b50e0c89bc0132d90fd94943803006757ca
eimer-castro-g <eimer.castro@globant.com>
2020-10-01 17:36:08 -0500

    Implementing flatmap example to converto objects to strings.

--- src/main/java/com/ewch/springboot/reactor/app/SpringbootReactorApplication.java
+++ src/main/java/com/ewch/springboot/reactor/app/SpringbootReactorApplication.java
@@ -4,6 +4,7 @@ import com.ewch.springboot.reactor.app.model.User;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
@@ -24,10 +25,41 @@ public class SpringbootReactorApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		// iterableFlux();
-		flatmatExample();
+		// flatmatExample();
+		flatmatFromUserToString();
 	}
 
-	public void flatmatExample() throws Exception {
+	public void flatmatFromUserToString() throws Exception {
+		String names[] = {"Eimer Castro", "Alejandra Marin", "Manuel Castro", "Luisa Hincapie", "Duvan Castro", "Bruce Lee", "Bruce Willis"};
+		List<User> namesList = Arrays.stream(names)
+			.map(name -> new User(name.split(" ")[0].toUpperCase(), name.split(" ")[1].toUpperCase()))
+			.collect(Collectors.toList());
+
+		Flux.fromIterable(namesList)
+			// Using map operator to convert a String in a User object
+			.map(user -> user.getFirstName().toUpperCase().concat(" ").concat(user.getLastName()).toUpperCase())
+			// Using filter operator to get users with a pattern
+			.flatMap(fullName -> {
+				if (fullName.contains("Bruce".toUpperCase())) {
+					return Mono.just(fullName);
+				} else {
+					return Mono.empty();
+				}
+			})
+			// Using map operator to transform a User's attribute value into another one (String to lowercase)
+			.map(String::toLowerCase)
+			// Subscribing users because observables are immutable. To get a new data it's important create a new object.
+			.subscribe(
+				// Main method  to execute in callback
+				user -> LOGGER.info(user.toString()),
+				// Error handler
+				error -> LOGGER.error(error.getMessage()),
+				// onComplete method to execute when callback finishes
+				() -> LOGGER.info("Execution has finished successfully!")
+			);
+	}
+
+	public void flatmatFromStringToUser() throws Exception {
 		String names[] = {"Eimer Castro", "Alejandra Marin", "Manuel Castro", "Luisa Hincapie", "Duvan Castro", "Bruce Lee", "Bruce Willis"};
 		List<String> namesList = new ArrayList<>(Arrays.asList(names));
 
